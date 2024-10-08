3d67786c232230f9b9221487a6bbf2d505af3b06
Yeshi1320 <yeshi2207@gmail.com>
2024-10-08 08:13:47 +0530

    Practice on JavaScript

---
+++ JavaScript/Script/prompt.js
@@ -0,0 +1,16 @@
+function checkEligibility() {
+       
+    let age = prompt("Please enter your age:");
+    age = Number(age);
+    let message;
+    if (isNaN(age) || age < 0) {
+        message = "The age is invalid if it is a negative number.";
+    } else if (age >= 18) {
+        message = "User is eligible to vote.";
+    } else {
+        message = "User is not eligible to vote because the age is less than 18.";
+    }
+
+   
+    document.getElementById("p1").innerText = message;
+}
